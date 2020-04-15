7b065c965c3d982e801d46dacac8008882cf6752
Michael <michalp2404@onet.pl>
2020-04-15 22:40:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ url = 'https://www.nbp.pl/home.aspx?f=/kursy/kursyc.html'
 headers = {'User-Agent': 'Mozilla/5.0'}
 response = requests.get(url, headers=headers)
  
+soup = BeautifulSoup(response.text, 'lxml')
 for s in soup.find_all('table', attrs={'class' : 'pad5'}):
     for row, tr in enumerate(s.find_all('tr')):
         for col, td in enumerate(tr.find_all('td')):
