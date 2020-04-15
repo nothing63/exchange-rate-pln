7223162c12cb75d8c67ccb1a87c9f1bef89d9e74
Michael <michalp2404@onet.pl>
2020-04-15 22:39:49 +0200

    Create scraper

---
+++ main.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from bs4 import BeautifulSoup
+import requests
+
+tk=Tk()
+tk.title("Exchange rate PLN")
+tk.resizable(False, False)
+
+Label(tk, padx=5, pady=5, text="Name (Polish): ", bg="#006dbc", font="bold").grid(row=0, column=0)
+Label(tk, padx=5, pady=5, text="Code: ", bg="#006dbc", font="bold").grid(row=0, column=1)
+Label(tk, padx=5, pady=5, text="Sell: ", bg="#006dbc", font="bold").grid(row=0, column=2)
+Label(tk, padx=5, pady=5, text="Buy: ", bg="#006dbc", font="bold").grid(row=0, column=3)
+
+url = 'https://www.nbp.pl/home.aspx?f=/kursy/kursyc.html'
+ 
+headers = {'User-Agent': 'Mozilla/5.0'}
+response = requests.get(url, headers=headers)
+ 
+for s in soup.find_all('table', attrs={'class' : 'pad5'}):
+    for row, tr in enumerate(s.find_all('tr')):
+        for col, td in enumerate(tr.find_all('td')):
+            Label(tk, padx=5, pady=5, text=td.text).grid(row=row, column=col)
+
+tk.mainloop()
